7fca78f8fd7a8e20dfff8fd0afc35e5969827660
Yaren Çetinkaya <20050151001@ybu.edu.tr>
2023-05-01 21:43:43 +0300

    juiciness

---
+++ The Graduation Project/Assets/Scripts/Yaren/JuicyEffect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuicyEffect : MonoBehaviour
+{
+    public float verticalOffset = 0.1f; // Kaç birim yukarı-aşağı hareket edeceği
+    
+    private Vector3 originalPosition;
+    
+    
+
+    private void Start()
+    {
+        // Sprite'ın orijinal pozisyonunu kaydediyoruz
+        originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Sprite'ı verticalOffset birim yukarı ve aşağı hareket ettiriyoruz
+        transform.position = new Vector3(originalPosition.x, originalPosition.y + Mathf.Sin(Time.time * 5) * verticalOffset, originalPosition.z);
+    }
+}
