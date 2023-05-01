a741c5c8e1bd0242388817ca94571b1664534512
Yaren Çetinkaya <20050151001@ybu.edu.tr>
2023-05-02 00:49:24 +0300

    Camera Controller done

--- The Graduation Project/Assets/Scripts/Yaren/CameraController.cs
+++ The Graduation Project/Assets/Scripts/Yaren/CameraController.cs
@@ -1,9 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject camera;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void LeftArrow()
+    {
+        if(camera.transform.position.x > -52) camera.transform.position = new Vector3(camera.transform.position.x - 18, camera.transform.position.y, -10f);
+    }
     
+    public void RightArrow()
+    {
+        if(camera.transform.position.x < 52) camera.transform.position = new Vector3(camera.transform.position.x + 18, camera.transform.position.y, -10f);
+    }
     
+    public void TopArrow()
+    {
+        if(camera.transform.position.y < 30) camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + 10, -10f);
+    }
+    
+    public void BottomArrow()
+    {
+        if(camera.transform.position.y > -30) camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - 10, -10f);
+    }
 }
